d3ea6eb1b0b3342c92e6217bdf728b9843c60a62
Kumar Bishwajeet <bishwajeetk23@gmail.com>
2024-05-14 00:30:36 +0530

    Unlimited Color project added

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,7 +1,7 @@
 const clock=document.getElementById('clock');
-
-setInterval(()=>{
+const curtime=()=>{
     const date=new Date();
     clock.innerHTML=date.toLocaleTimeString('en-US');
-},1000);
+}
+setInterval(curtime,1000);
 
